d17336ba85073a950428c6746a1e66d92795a186
DOH-KEW4303 <kristen.waterman@doh.wa.gov>
2025-06-12 11:10:24 -0700

    Add connectionto duckdb and function to write new anon id's to table

--- scripts/seq_ID_assign.R
+++ scripts/seq_ID_assign.R
@@ -8,6 +8,8 @@ library(readr)
 library(tidyverse)
 library(duckdb)
 
+readRenviron("../.Renviron")  #load renviron file here
+
 # Credentials
 starLIMS_path <- Sys.getenv("STARLIMS_PATH")
 db_info <- read_tsv(starLIMS_path)
@@ -19,7 +21,7 @@ basespace_id <- basespace_id %>%
   select(-bs_id)#extract only WA id
 
 # Connection
-con <- DBI::dbConnect(odbc::odbc(),
+lims_con <- DBI::dbConnect(odbc::odbc(),
                       Driver = "SQL Server Native Client 11.0",
                       Server = db_info$server,
                       Database = db_info$database,
@@ -36,12 +38,69 @@ results <- basespace_id %>%
       "FROM ", db_info$database, ".dbo.", db_info$table, " ",
       "WHERE PHLAccessionNumber = '", id, "'"
     )
-    dbGetQuery(con, sql_query)
+    dbGetQuery(lims_con, sql_query)
   })) %>%
   unnest(lims_info)
 
-#add anon id col to dataframe
+# Get path from .Renviron
+db_path <- Sys.getenv("DUCKDB_PATH")
+
+# Create/connect to the DuckDB file and create table
+assign_anon_ids <- function(results, db_path) {
+  # Connect to duckdb (read-only = FALSE because we're inserting)
+  con <- dbConnect(duckdb::duckdb(), dbdir = db_path, read_only = FALSE)
+  
+  # Create table if it doesn't exist yet
+  dbExecute(con, "
+    CREATE TABLE IF NOT EXISTS anon_ids (
+      wa_id TEXT PRIMARY KEY,
+      anon_id TEXT,
+      year_collected INTEGER
+    )
+  ")
+  # Loop through results df and assign anon_id if not already in DB
+  results<- results %>%
+    rowwise() %>%
+    mutate(
+      anon_id = {
+        existing_id <- dbGetQuery(con, paste0(
+          "SELECT anon_id FROM anon_ids WHERE wa_id = '", wa_id, "'"
+        ))
+        
+        if (nrow(existing_id) > 0) {
+          existing_id$anon_id
+        } else {
+          # Generate new ID
+          year <- year(as.Date(SpecimenDateCollected))
+          repeat {
+            rand_num <- sprintf("%06d", sample(1e6, 1))
+            new_anon_id <- paste0("Mtb/Homo sapiens/USA/WA-PHL-", rand_num, "/", year)
+            
+            # Check if random ID is already used
+            existing <- dbGetQuery(con, paste0(
+              "SELECT 1 FROM anon_ids WHERE anon_id = '", new_anon_id, "'"
+            ))
+            
+            if (nrow(existing) == 0) {
+              # Insert into DuckDB
+              dbExecute(con, "INSERT INTO anon_ids (wa_id, anon_id, year_collected) VALUES (?, ?, ?)", 
+                        params = list(wa_id, new_anon_id, year))
+              break
+            }
+          }
+          new_anon_id
+        }
+      }
+    ) %>%
+    ungroup()
+  
+  dbDisconnect(con)
+  return(results)
+}
+
+#call the function to assign ID's 
+results <- assign_anon_ids(results, db_path)
+view(results)
 
 
 
-print(results)
